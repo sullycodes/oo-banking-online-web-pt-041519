b1e3a37b0a705a335bb3f3f1f0d5f9a7d511fc36
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 17:37:03 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
+  
+  attr_accessor :account, :balance, :status
+  attr_reader :name
+  
+  def initialize(account)
+    @account = account
+    @name = account
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(deposit)
+    @balance = @balance + deposit
+  end
+  
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
 
+  #need valid? to return true if more than zero or open, need to return false if balnce is 0 or status is closed
+  def valid?
+    if @balance == 0 || @status == "closed"
+      return false
+    else
+      true
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+  
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,99 @@
 class Transfer
-  # your code here
-end
+  
+  attr_accessor :sender, :receiver, :transfer, :status, :amount
+  
+  @@executed_transactions = []
+  
+  def initialize(sender, receiver, amount)
+    # @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    if receiver.valid? == true && sender.valid? == true
+      return true
+    else
+      return false 
+    end
+  end
+  
+  def execute_transaction
+    if @sender.balance == 0 || @sender.status == "closed" || @amount > @sender.balance
+    # if @sender.valid? 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      if @@executed_transactions.none? { |e| e == self }  
+        @sender.balance = @sender.balance - @amount
+        @receiver.balance = @receiver.balance + @amount
+        @status = "complete"
+        @@executed_transactions << self
+      end
+    end
+  end
+  
+  # def execute_transaction
+  #   if @sender.valid? 
+  #     if @@executed_transactions.none? { |e| e == self }  
+  #       @sender.balance = @sender.balance - @amount
+  #       @receiver.balance = @receiver.balance + @amount
+  #       @status = "complete"
+  #     end
+  #   else
+  #     @@executed_transactions << self
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance." 
+  #   end
+  # end
+  
+  
+  def reverse_transfer
+    @@executed_transactions.each do |e|
+      if e == self
+        @sender.balance = @sender.balance + @amount
+        @receiver.balance = @receiver.balance - @amount
+        @status = "reversed"
+      end
+    end
+  end
+  
+end #Transfer
+
+class BankAccount
+  
+  attr_accessor :account, :balance, :status
+  attr_reader :name
+  
+  def initialize(account)
+    @account = account
+    @name = account
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(deposit)
+    @balance = @balance + deposit
+  end
+  
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance == 0 || @status == "closed"
+      return false
+    else
+      true
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+  
+end #BankAccount
+
+
