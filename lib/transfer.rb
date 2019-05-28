5d8554200dba0326879f24d8ec017be597b31660
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 17:19:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,8 @@ class Transfer
   end
   
   def execute_transaction
-    # if @sender.balance < 1 || @sender.status == "closed" 
-    if self.valid? 
+    if @sender.balance < 1 || @sender.status == "closed" 
+    # if self.valid? 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     else
