9cbd858d1d501e1eb7ae0cd4a1d7f08196f096cd
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 16:47:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,84 @@
 class Transfer
-  # your code here
-end
+  
+  attr_accessor :sender, :receiver, :transfer, :status, :amount
+  
+  @@executed_transactions = []
+  
+  def initialize(sender, receiver, amount)
+    # @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    if receiver.valid? == true && sender.valid? == true
+      return true
+    else
+      return false 
+    end
+  end
+  
+  def execute_transaction
+    # if self.valid? == false
+    if @sender.balance < 1 || @sender.status == "closed" 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      if @@executed_transactions.none? { |e| e == self }  
+        @sender.balance = @sender.balance - @amount
+        @receiver.balance = @receiver.balance + @amount
+        @status = "complete"
+        @@executed_transactions << self
+      end
+    end
+  end
+  
+  def reverse_transfer
+    @@executed_transactions.each do |e|
+      if e == self
+        @sender.balance = @sender.balance + @amount
+        @receiver.balance = @receiver.balance - @amount
+        @status = "reversed"
+      end
+    end
+  end
+  
+end #Transfer
+
+class BankAccount
+  
+  attr_accessor :account, :balance, :status
+  attr_reader :name
+  
+  def initialize(account)
+    @account = account
+    @name = account
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(deposit)
+    @balance = @balance + deposit
+  end
+  
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance == 0 || @status == "closed"
+      return false
+    else
+      true
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+  
+end #BankAccount
+
+
